cf6fbeaae21221ba462099f89c25cf6e4db7f3da
kant9612 <kanstephentakayama@gmail.com>
2021-03-03 10:48:53 +0000

    [add] 新規登録/ログイン/ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # ユーザー登録・ログイン認証の前にconfigure_permitted_parametersを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 呼び出された他のコントローラからも参照可能
+  protected
+  def configure_permitted_parameters
+  # sign_upの時にname（ユーザー名）を操作可能に
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # ログイン機能のためdeviseを導入
+  devise_for :users
+
   root to: 'homes#top'
 end
