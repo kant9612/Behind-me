71ef10a8962f37c372b0942c58d44ed958f80e04
kant9612 <kanstephentakayama@gmail.com>
2021-03-11 09:27:21 +0000

    [fix]homes/top

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
     @lists = List.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
+    # @list.user_id = current_user.id
   end
   def about
   end
