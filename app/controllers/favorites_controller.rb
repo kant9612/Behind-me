9bc20a1d9017a127516f604a628c2b2dde70ad7b
kant9612 <kanstephentakayama@gmail.com>
2021-03-13 04:17:06 +0000

    [add]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,11 @@ class FavoritesController < ApplicationController
     @list = List.find(params[:list_id])
     favorite = @list.favorites.new(user_id: current_user.id)
     favorite.save
-    # redirect_to request.referer
   end
 
   def destroy
     @list = List.find(params[:list_id])
     favorite = @list.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    # redirect_to request.referer
   end
 end
