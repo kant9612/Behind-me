fb710e29f5a1f28c2b64404821beeccd6897bff0
kant9612 <kanstephentakayama@gmail.com>
2021-03-18 03:29:53 +0000

    [fixing]検索から完全一致を削除

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,32 +4,33 @@ class SearchController < ApplicationController
 	def search
     @model = params["search"]["model"]
     @content = params["search"]["content"]
-    @how = params["search"]["how"]
-    @datas = search_for(@how, @model, @content)
+    # @how = params["search"]["how"]
+    @datas = search_for(@model, @content)
   end
 
   private
 
-  def match(model, content)
-    if model == 'user'
-      User.where(name: content)
-    elsif model == 'list'
-      List.joins(:list_categories).where('list_categories.category_id = ?', content)
-    end
-  end
+  # def match(model, content)
+  #   if model == 'user'
+  #     User.where(name: content)
+  #   elsif model == 'list'
+  #     List.joins(:list_categories).where('list_categories.category_id = ?', content)
+  #   end
+  # end
   def partical(model, content)
     if model == 'user'
       User.where("name LIKE ?", "%#{content}%")
     elsif model == 'list'
       List.where("list_title LIKE ?", "%#{content}%")
+      # List.joins(:list_categories).where('list_categories.category_id = ?', content)
     end
   end
-  def search_for(how, model, content)
-    case how
-    when 'match'
-      match(model, content)
-    when 'partical'
-      partical(model, content)
-    end
+  def search_for(model, content)
+  #   case how
+  #   # when 'match'
+  #   #   match(model, content)
+  #   when 'partical'
+  #     partical(model, content)
+  #   end
   end
 end
