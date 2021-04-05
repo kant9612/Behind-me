62b3cb8e25148c6e78544793acb9b1079dad722a
kant9612 <kanstephentakayama@gmail.com>
2021-04-05 00:51:44 +0000

    [fix]微調整

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,8 @@ class HomesController < ApplicationController
   def top
     @lists = List.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
     @lists = Kaminari.paginate_array(@lists).page(params[:page]).per(8)
-    # @list.user_id = current_user.id
   end
+
   def about
   end
 end
